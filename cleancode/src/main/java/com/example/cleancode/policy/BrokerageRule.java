83a6d0e261c79af595d7227b00d4a8d9fc25bf87
backtony <cjs1863@gmail.com>
2021-10-01 22:41:43 +0900

    클래스

--- cleancode/src/main/java/com/example/cleancode/policy/BrokerageRule.java
+++ cleancode/src/main/java/com/example/cleancode/policy/BrokerageRule.java
@@ -2,22 +2,26 @@ package com.example.cleancode.policy;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.lang.Nullable;
 
 /**
  * 가격이 특정 범위일 때 상한효율과 상한 금액이 가지는 클래스
  */
 @AllArgsConstructor
+@Getter
 public class BrokerageRule {
+    private Long lessThan;
     private Double brokeragePercent;
 
     @Nullable // null이 들어올 수 있다는 주석 annotation
     private Long limitAmount;
 
+    public BrokerageRule(Long lessThan, Double brokeragePercent) {
+        this(lessThan,brokeragePercent,Long.MAX_VALUE);
+    }
+
     public Long cacMaxBrokerage(Long price){
-        if (limitAmount == null){
-            return multiplyPercent(price);
-        }
         return Math.min(multiplyPercent(price),limitAmount);
     }
 
